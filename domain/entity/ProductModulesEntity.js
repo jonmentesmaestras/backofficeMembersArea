3732e9a09e8bcd9f99a94239ddcce85f01d706ba
jonmentesmaestras <jon.mosquera@gmail.com>
2024-01-24 14:58:15 -0500

    adding ProductModulesEntity.js

---
+++ domain/entity/ProductModulesEntity.js
@@ -0,0 +1,12 @@
+class ProductModulesEntity {
+    ProductModuleID = 0;
+    ProductModuleCode = "";
+    ProductModuleName = "";
+    FK_ProductCode = "";
+    Active = false;
+    ActivedDT = null;
+    CreatedBy = "";
+    CreatedDT = null;
+    UpdatedBy = "";
+    UpdatedDT = null;
+}
